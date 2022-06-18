04d1544ebc1aedb54036183aec9eecec57967aae
Maksym_Yakubov <maksym_yakubov@epam.com>
2022-06-19 00:00:44 +0300

    Add lodash.camelcase dependency

--- src/camelize.ts
+++ src/camelize.ts
@@ -1,3 +1,5 @@
+import camelCase from 'lodash.camelcase';
+
 export function camelize(str: string): string {
-  return str.replace(/^([A-Z])|[\s-_](\w)/g, (match, p1, p2) => (p2 ? p2.toUpperCase() : p1.toLowerCase()));
+  return camelCase(str);
 }
